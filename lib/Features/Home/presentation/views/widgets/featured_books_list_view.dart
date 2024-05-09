ae6f35c10fd050cfd02e781197c77e06e65128c2
Ali-Elchab <alielchab01@gmail.com>
2024-05-09 17:48:19 +0300

    Created styles file & add title to home view

--- lib/Features/Home/presentation/views/widgets/featured_books_list_view.dart
+++ lib/Features/Home/presentation/views/widgets/featured_books_list_view.dart
@@ -1,4 +1,4 @@
-import 'package:bookly_app/Features/Home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:bookly_app/Features/Home/presentation/views/widgets/featured_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
@@ -13,7 +13,7 @@ class FeaturedBooksListView extends StatelessWidget {
           itemBuilder: (context, index) {
             return const Padding(
               padding: EdgeInsets.all(8.0),
-              child: CustomListViewItem(),
+              child: FeaturedListViewItem(),
             );
           }),
     );
--- lib/Features/Home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/Home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/Features/Home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/Features/Home/presentation/views/widgets/featured_books_list_view.dart';
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -7,11 +8,20 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        FeaturedBooksListView(),
-      ],
+    return const Padding(
+      padding: EdgeInsets.all(24.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(height: 40.0),
+          Text(
+            'Best Sellers',
+            style: Styles.titleMedium,
+          ),
+        ],
+      ),
     );
   }
 }
