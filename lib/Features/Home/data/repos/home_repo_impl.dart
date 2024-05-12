67d0f7bad14261bf39fd6adde30b8270344cea7e
Ali-Elchab <alielchab01@gmail.com>
2024-05-13 00:29:19 +0300

    Fetched similar books

--- lib/Features/Home/data/repos/home_repo_impl.dart
+++ lib/Features/Home/data/repos/home_repo_impl.dart
@@ -64,30 +64,30 @@ class HomeRepoImpl implements HomeRepo {
     }
   }
 
-  // @override
-  // Future<Either<Failure, List<BookModel>>> fetchSimilarBooks(
-  //     {required String category}) async {
-  //   try {
-  //     var data = await apiService.get(
-  //         path:
-  //             'volumes?Filtering=free-ebooks&Sorting=relevance &q=subject:Programming');
-  //     List<BookModel> books = [];
-  //     for (var item in data['items']) {
-  //       books.add(BookModel.fromJson(item));
-  //     }
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks(
+      {required String category}) async {
+    try {
+      var data = await apiService.get(
+          path:
+              'volumes?Sorting=relevance&Filtering=free-ebooks&q=subject:Programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
 
-  //     return right(books);
-  //   } catch (e) {
-  //     if (e is DioException) {
-  //       return left(
-  //         ServerFailure.fromDioException(e),
-  //       );
-  //     }
-  //     return left(
-  //       ServerFailure(
-  //         e.toString(),
-  //       ),
-  //     );
-  //   }
-  // }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioException(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
+  }
 }
