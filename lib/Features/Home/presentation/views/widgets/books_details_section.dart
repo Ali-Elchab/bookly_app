058737530f0136d676109b2af0019bee2b9ca278
Ali-Elchab <alielchab01@gmail.com>
2024-05-12 10:36:43 +0300

    Refactored books details view

---
+++ lib/Features/Home/presentation/views/widgets/books_details_section.dart
@@ -0,0 +1,41 @@
+import 'package:bookly_app/Features/Home/presentation/views/widgets/book_rating.dart';
+import 'package:bookly_app/Features/Home/presentation/views/widgets/custom_book_image.dart';
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class BooksDetailsSection extends StatelessWidget {
+  const BooksDetailsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+          child: const CustomBookImage(),
+        ),
+        const SizedBox(height: 43.0),
+        Text(
+          'The Jungle Book',
+          style: Styles.textStyle30,
+        ),
+        const SizedBox(height: 6.0),
+        Opacity(
+          opacity: 0.7,
+          child: Text(
+            'Rudyard Kipling',
+            style: Styles.textStyle18.copyWith(
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        const SizedBox(height: 18.0),
+        const BookRating(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ],
+    );
+  }
+}
